aae86f84cdc21a4730b5ee40bc6e8d308fc7e01c
clément Géhin <clement.gehin@4murs.fr>
2025-02-18 13:59:55 +0100

    initialisation + debut de projet

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "leaflet/dist/leaflet.css"; 
+  import Header from './components/Header.svelte';
+  import HeroSection from './components/HeroSection.svelte';
+  import Map from './components/Map.svelte';
+  import Footer from './components/Footer.svelte';
+  import OnSpace from './components/OnSpace.svelte';
+</script>
+
+<Header />
+<HeroSection />
+<OnSpace />
+<Map />
+<Footer />
+
---
+++ src/components/Footer.svelte
@@ -0,0 +1,29 @@
+<footer>
+  <ul>
+    <li><a href="#">Facebook</a></li>
+    <li><a href="#">Twitter</a></li>
+    <li><a href="#">Instagram</a></li>
+  </ul>
+  <p>&copy; 2025 Espace Explorer. Tous droits réservés.</p>
+</footer>
+
+<style>
+  footer {
+    background-color: #0a0b12;
+    color: white;
+    padding: 20px;
+    text-align: center;
+  }
+
+  footer ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: center;
+    gap: 15px;
+  }
+
+  footer a {
+    text-decoration: none;
+    color: white;
+  }
+</style>
